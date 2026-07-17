459ea9ad45a0172a20e7d5774a59c141e7bb2ba9
Mark Zhu <markzhu926@gmail.com>
2026-07-17 00:58:57 -0400

    add annotations and imports

--- src/main/java/com/example/taskmanager/entity/Task.java
+++ src/main/java/com/example/taskmanager/entity/Task.java
@@ -1,8 +1,21 @@
 package com.example.taskmanager.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int taskId;
     private String task;
+
+    @Enumerated(EnumType.STRING)
     private Priority priority;
 
     public Task(String task, Priority priority){
